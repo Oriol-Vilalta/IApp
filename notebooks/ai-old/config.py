a2b192cc1c2cfa0d429602c1713a747912ef8499
Oriol-Vilalta <120737270+Oriol-Vilalta@users.noreply.github.com>
2025-01-23 20:36:01 +0100

    Update Gitignore and deleted old Files

--- notebooks/ai-old/config.py
+++
@@ -1,9 +0,0 @@
-DATA_DIR = "/backend/.data"
-
-DEFAULT_CONFIG = {
-    'state': 'NEW',
-    'desc': '',
-}
-
-ID_LEN = 10
-
--- notebooks/ai-old/installation.py
+++
@@ -1,9 +0,0 @@
-import os
-from .config import DATA_DIR
-
-# Prepare the environment for the execution of the program
-# Does the pre execution routine, like generate folders
-
-def install():
-    os.makedirs(f'{DATA_DIR}', exist_ok=True)
-    os.makedirs(f'{DATA_DIR}/.tmp', exist_ok=True)
